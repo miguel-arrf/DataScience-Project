92d2b3a342d73352ad417295944c340b7f20695a
miguel-arrf <mferreira.oeiras@gmail.com>
2021-12-03 14:06:07 +0000

    Initial commit, let's do it :D

---
+++ DataDistribution_Set2/BoxPlots.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import ds_charts as ds
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+filename = '/Users/miguelferreira/Documents/IST/1Ano/2P/CD/air_quality_tabular.csv'
+data = pd.read_csv(filename, index_col='date', na_values='', parse_dates=True, infer_datetime_format=True)
+
+print(data.isna().sum() / (len(data)) * 100)  # Relative missing values
+print("--")
+print(data.isna().sum())  # Absolute missing values
+
+# Printing box plots for each numeric variable
+numeric_data = data.select_dtypes(include=np.number)
+print(numeric_data.columns.tolist())
+
+rows, cols = ds.choose_grid(len(numeric_data.columns.tolist()))
+fig, axs = plt.subplots(rows, cols, figsize=(cols * ds.HEIGHT, rows * ds.HEIGHT))
+i, j = 0, 0
+for n in range(len(numeric_data.columns.tolist())):
+    print("for: {}".format(numeric_data.columns.tolist()[n]))
+    axs[i, j].set_title('Boxplot for %s' % numeric_data.columns.tolist()[n])
+    axs[i, j].boxplot(data[numeric_data.columns.tolist()[n]].dropna().values)
+    i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
+plt.show()
+
+
---
+++ DataDistribution_Set2/Outliers.py
@@ -0,0 +1,96 @@
+import numpy as np
+from matplotlib.pyplot import figure, show, subplots, Axes
+from ds_charts import multiple_bar_chart, HEIGHT, multiple_line_chart
+import pandas as pd
+from pandas.plotting import register_matplotlib_converters
+import ds_charts as ds
+from seaborn import distplot
+from numpy import log
+from pandas import Series
+from scipy.stats import norm, expon, lognorm
+
+register_matplotlib_converters()
+filename = '/Users/miguelferreira/Documents/IST/1Ano/2P/CD/air_quality_tabular.csv'
+data = pd.read_csv(filename, index_col='date', na_values='', parse_dates=True, infer_datetime_format=True)
+
+NR_STDEV: int = 2
+
+data = data.drop('FID', axis=1)
+
+numeric_data = data.select_dtypes(include=np.number)
+numeric_vars = numeric_data.columns.tolist()
+if [] == numeric_vars:
+    raise ValueError('There are no numeric variables.')
+
+outliers_iqr = []
+outliers_stdev = []
+summary_5 = data.describe(include='number')
+
+for var in numeric_vars:
+    iqr = 1.5 * (summary_5[var].quantile(q=0.75) - summary_5[var].quantile(q=0.25))
+    outliers_iqr += [
+        data[data[var] > summary_5[var].quantile(q=0.75) + iqr].count()[var] +
+        data[data[var] < summary_5[var].quantile(q=0.25) - iqr].count()[var]]
+    std = NR_STDEV * summary_5[var].std()
+    outliers_stdev += [
+        data[data[var] > summary_5[var].mean() + std].count()[var] +
+        data[data[var] < summary_5[var].mean() - std].count()[var]]
+
+outliers = {'iqr': outliers_iqr, 'stdev': outliers_stdev}
+figure(figsize=(12, HEIGHT))
+multiple_bar_chart(numeric_vars, outliers, title='Nr of outliers per variable', xlabel='variables',
+                   ylabel='nr outliers', percentage=False)
+show()
+
+print(outliers_iqr)
+print("--")
+print(outliers_stdev)
+
+rows, cols = ds.choose_grid(len(numeric_vars))
+print("rows: {}, cols: {}".format(rows, cols))
+fig, axs = subplots(rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT))
+i, j = 0, 0
+for n in range(len(numeric_vars)):
+    axs[i, j].set_title('Histogram for %s' % numeric_vars[n])
+    axs[i, j].set_xlabel(numeric_vars[n])
+    axs[i, j].set_ylabel("nr records")
+    axs[i, j].hist(data[numeric_vars[n]].dropna().values, 'auto')
+    i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
+show()
+
+fig, axs = subplots(rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT))
+i, j = 0, 0
+for n in range(len(numeric_vars)):
+    axs[i, j].set_title('Histogram with trend for %s' % numeric_vars[n])
+    distplot(data[numeric_vars[n]].dropna().values, norm_hist=True, ax=axs[i, j], axlabel=numeric_vars[n])
+    i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
+show()
+
+
+def compute_known_distributions(x_values: list) -> dict:
+    distributions = dict()
+    # Gaussian
+    mean, sigma = norm.fit(x_values)
+    distributions['Normal(%.1f,%.2f)' % (mean, sigma)] = norm.pdf(x_values, mean, sigma)
+    # Exponential
+    loc, scale = expon.fit(x_values)
+    distributions['Exp(%.2f)' % (1 / scale)] = expon.pdf(x_values, loc, scale)
+    # LogNorm
+    sigma, loc, scale = lognorm.fit(x_values)
+    distributions['LogNor(%.1f,%.2f)' % (log(scale), sigma)] = lognorm.pdf(x_values, sigma, loc, scale)
+    return distributions
+
+
+def histogram_with_distributions(ax: Axes, series: Series, var: str):
+    values = series.sort_values().values
+    ax.hist(values, 20, density=True)
+    distributions = compute_known_distributions(values)
+    multiple_line_chart(values, distributions, ax=ax, title='Best fit for %s' % var, xlabel=var, ylabel='')
+
+
+fig, axs = subplots(rows, cols, figsize=(cols * HEIGHT, rows * HEIGHT))
+i, j = 0, 0
+for n in range(len(numeric_vars)):
+    histogram_with_distributions(axs[i, j], data[numeric_vars[n]].dropna(), numeric_vars[n])
+    i, j = (i + 1, 0) if (n + 1) % cols == 0 else (i, j + 1)
+show()
