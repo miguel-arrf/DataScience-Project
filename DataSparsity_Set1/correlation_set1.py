05515bd552519ad638821e8770232395e89551d8
miguel-arrf <mferreira.oeiras@gmail.com>
2021-12-05 01:31:51 +0000

    Added images for distribution fitting, granularity; granularity with multiple sized bins; sparsity graph only for relevant symbolic features and with histogram on the diagonal!
    correlation_set1.py:
    - Now uses relevant features.
    
    fittedDistribution.py:
    - For generating fitted distribution graphs for relevant features.
    
    granularity_study_set1.py:
    - Minimum bin size is now lower (2 and 3).

--- DataSparsity_Set1/correlation_set1.py
+++ DataSparsity_Set1/correlation_set1.py
@@ -4,6 +4,7 @@ from pandas import read_csv
 from pandas.plotting import register_matplotlib_converters
 from matplotlib.pyplot import savefig, show, title, figure
 from seaborn import heatmap
+import seaborn as sns
 
 register_matplotlib_converters()
 filename = '../data/NYC_collisions_tabular.csv'
@@ -18,8 +19,6 @@ title('Correlation analysis')
 savefig(f'../DataSparsity_Set1/images/correlation_analysis.png')
 show()
 
-import seaborn as sns
-
 '''
 # Basic correlogram
 sns.pairplot(data, vars=data.columns[1:3])
@@ -28,24 +27,37 @@ savefig(f'../DataSparsity_Set1/images/teste.png')
 show()
 '''
 
-data = data[["BODILY_INJURY","SAFETY_EQUIPMENT","PERSON_SEX"]]
+features_to_use = ["BODILY_INJURY", "SAFETY_EQUIPMENT", "PERSON_SEX", "PED_LOCATION", "CONTRIBUTING_FACTOR_2",
+                   "EJECTION", "COMPLAINT", "EMOTIONAL_STATUS", "CONTRIBUTING_FACTOR_1", "POSITION_IN_VEHICLE",
+                   "PED_ACTION", "PERSON_INJURY"]
+
+data = data[features_to_use]
+
+for feature in features_to_use:
+    data[feature] = data[feature].astype(str)
+
 data['BODILY_INJURY'] = data.BODILY_INJURY.astype(str)
 data['SAFETY_EQUIPMENT'] = data.SAFETY_EQUIPMENT.astype(str)
 data['PERSON_SEX'] = data.PERSON_SEX.astype(str)
 
 print(data.dtypes)
-# create customized scatterplot that first filters out NaNs in feature pair
+
+
+# TODO: This was taken from somewhere over stackoverflow, it is important to figure out from where and refer it on
+#  the report! create customized scatterplot that first filters out NaNs in feature pair
 def scatterFilter(x, y, **kwargs):
     interimDf = pd.concat([x, y], axis=1)
     interimDf.columns = ['x', 'y']
     interimDf = interimDf[(~ pd.isnull(interimDf.x)) & (~ pd.isnull(interimDf.y))]
 
     ax = plt.gca()
-    ax = plt.plot(interimDf.x.values, interimDf.y.values, 'o', **kwargs)
+    # print(interimDf.x.values)
+    ax = sns.stripplot(x=interimDf.x.values, y=interimDf.y.values, jitter=True, palette='viridis')
+    # ax = plt.scatter(interimDf.x.values, interimDf.y.values, 'o', jitter=True, **kwargs)
 
 
 # Create an instance of the PairGrid class.
-grid = sns.PairGrid(data=data, vars=list(data.columns), size=4)
+grid = sns.PairGrid(data=data, vars=list(data.columns), height=4)
 
 # Map a scatter plot to the upper triangle
 grid = grid.map_upper(scatterFilter, color='darkred')
@@ -55,5 +67,7 @@ grid = grid.map_diag(plt.hist, bins=10, edgecolor='k', color='darkred')
 
 # Map a density plot to the lower triangle
 grid = grid.map_lower(scatterFilter, color='darkred')
+
+savefig(f'../DataSparsity_Set1/images/teste.png')
 
 plt.show()
